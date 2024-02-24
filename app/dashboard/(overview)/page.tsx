f7e8bc0c3cce05775a8ab4d9fea7e8df2d9d000f
Acto <chibthankgod@gmail.com>
2024-02-24 14:25:42 +0100

    fixing meta data

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -6,7 +6,7 @@ import { Suspense } from 'react';
 import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
 import { Metadata } from 'next';
 
-export const metadta:Metadata = {
+export const metadata:Metadata = {
   title:" Dashboard ",
 }
 
